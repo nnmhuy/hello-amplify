f686981a2ffd165a6d33098c19a2a0c3d3d6cf10
Minh Huy <nnmhuy@apcs.vn>
2019-09-03 16:54:22 +0700

    Updates

---
+++ precache-manifest.cd80f8385915c2d28951911da14f3bd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b82af8f0ee61371376a10612d3f38f",
+    "url": "/hello-amplify/index.html"
+  },
+  {
+    "revision": "ebb5707572190d88ca20",
+    "url": "/hello-amplify/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "b0f5e8c1589e1a5f9b9d",
+    "url": "/hello-amplify/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ebb5707572190d88ca20",
+    "url": "/hello-amplify/static/js/2.23b45eac.chunk.js"
+  },
+  {
+    "revision": "b0f5e8c1589e1a5f9b9d",
+    "url": "/hello-amplify/static/js/main.4fdbcd4f.chunk.js"
+  },
+  {
+    "revision": "281b6463aae9dd791b0c",
+    "url": "/hello-amplify/static/js/runtime~main.08725721.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-amplify/static/media/logo.5d5d9eef.svg"
+  }
+]);
